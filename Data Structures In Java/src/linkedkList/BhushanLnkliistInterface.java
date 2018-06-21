4d319ded8e3572f3277de4bf8c081675262f5fcf
Bhushan <Bhushan@10.0.0.35>
2018-06-20 23:12:23 -0700

    Created the interface for LinkedList

--- Data Structures In Java/src/linkedkList/BhushanLnkliistInterface.java
+++ Data Structures In Java/src/linkedkList/BhushanLnkliistInterface.java
@@ -8,29 +8,40 @@ package linkedkList;
  * @param <E>
  *
  */
-public interface BhushanLnkliistInterface {
+public interface BhushanLnkliistInterface<E> {
 
 	/*
-	 * This method is use to add element from first
-	 * Boundary conditions
-	 *  1. Empty list 
-	 *  2.Only one element in the list
-	 *  3.All other conditions will be eliminated as its just adding in first place
-	 *  
-	 *  Algorithm :  
-	 *  1.Create new node
-	 *  2.Point its next to the head
-	 *  3.Then point the head to new node created 
-	 *  Note: The pointer assignment sequence is most important   
+	 * This method is use to add element from first Boundary conditions 1. Empty
+	 * list 2.Only one element in the list 3.All other conditions will be eliminated
+	 * as its just adding in first place
+	 * 
+	 * Algorithm : 1.Create new node 2.Point its next to the head 3.Then point the
+	 * head to new node created Note: The pointer assignment sequence is most
+	 * important
 	 */
-	public void addFirst() ;
-	public void addLast() ;
-	public void removeFirst() ;
-	public void removeLast() ;
-	public void removeAndFind() ;
-	public void removePeek() ;
-		
+	public void addFirst(E obj);
 
-		
+	/*
+	 * This method is use to add element at last position in the list Boundary
+	 * conditions 1. Empty list -->Will get null pointer exception so make special
+	 * exception 2.Only one element in the list 3.All other conditions will be
+	 * eliminated as its just adding in first place
+	 * 
+	 * Algorithm : 1.Create new node 2.take temporary pointer and start traversing
+	 * from head to the end of list using while loop 3.Assign temporary next to the
+	 * new node created
+	 */
+	public void addLast(E obj);
+
+	public void removeFirst();
+
+	public void removeLast();
+
+	public void removeAndFind();
+
+	public void removePeek();
 	
+	public void displayList() ;
+
+
 }
--- Data Structures In Java/src/linkedkList/LinkedList.java
+++ Data Structures In Java/src/linkedkList/LinkedList.java
@@ -1,6 +1,6 @@
 package linkedkList;
 
-public class LinkedList<E> {
+public class LinkedList<E> implements BhushanLnkliistInterface<E> {
 
 	@SuppressWarnings("hiding")
 	class Node<E> {
@@ -25,15 +25,6 @@ public class LinkedList<E> {
 
 	}
 
-	/*
-	 * This method is use to add element from first Boundary conditions 1. Empty
-	 * list 2.Only one element in the list 3.All other conditions will be eliminated
-	 * as its just adding in first place
-	 * 
-	 * Algorithm : 1.Create new node 2.Point its next to the head 3.Then point the
-	 * head to new node created Note: The pointer assignment sequence is most
-	 * important
-	 */
 	public void addFirst(E obj) {
 		System.out.println("Inside of Add First Method");
 		Node<E> newNode = new Node<E>(obj);
@@ -42,16 +33,6 @@ public class LinkedList<E> {
 		currentSize++;
 	}
 
-	/*
-	 * This method is use to add element at last position in the list Boundary
-	 * conditions 1. Empty list -->Will get null pointer exception so make special
-	 * exception 2.Only one element in the list 3.All other conditions will be
-	 * eliminated as its just adding in first place
-	 * 
-	 * Algorithm : 1.Create new node 2.take temporary pointer and start traversing
-	 * from head to the end of list using while loop 3.Assign temporary next to the
-	 * new node created
-	 */
 	public void addLast(E obj) {
 		System.out.println("Inside of Add Last Method");
 		Node<E> newNode = new Node<E>(obj);
@@ -88,22 +69,46 @@ public class LinkedList<E> {
 
 	}
 
-	public void removeFirst(Object obj) {
+	public void removeFirst(E obj) {
 		// TODO Auto-generated method stub
 
 	}
 
-	public void removeLast(Object obj) {
+	public void removeLast(E obj) {
 		// TODO Auto-generated method stub
 
 	}
 
-	public void removeAndFind(Object obj) {
+	public void removeAndFind(E obj) {
 		// TODO Auto-generated method stub
 
 	}
 
-	public void removePeek(Object obj) {
+	public void removePeek(E obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeFirst() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeLast() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeAndFind() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removePeek() {
 		// TODO Auto-generated method stub
 
 	}
@@ -111,9 +116,9 @@ public class LinkedList<E> {
 	public static void main(String[] args) {
 		System.out.println("Calling main Method ");
 		LinkedList<Integer> list = new LinkedList<Integer>();
-		//list.addFirst(1);
-		//list.addFirst(2);
-		//list.addFirst(3);
+		// list.addFirst(1);
+		// list.addFirst(2);
+		// list.addFirst(3);
 		list.addLast(1);
 		list.addLast(2);
 		list.addLast(3);
